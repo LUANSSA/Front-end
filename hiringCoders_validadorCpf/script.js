9bd26c83b0dc2366ec2ea95a164a594928aa7545
Luan Souza <alunoluansenai@gmail.com>
2022-05-15 18:12:05 -0300

    Atualizando validador de CPF

--- hiringCoders_validadorCpf/script.js
+++ hiringCoders_validadorCpf/script.js
@@ -1,1 +1,20 @@
 console.log("JavaScript");
+function validar(){
+    var cpf = document.getElementById("cpf_input").value;
+    console.log(cpf);
+
+    var resultadoValidacao = validarCpf(cpf);
+
+    if(resultadoValidacao){
+        var success = document.getElementById("success");
+        success.style.display="block";
+    }else{
+        var error = document.getElementById("error");
+        error.style.display="block";
+    }
+}
+
+function validarCpf(cpf){
+
+    return false;
+}
